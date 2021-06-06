ae169b94bef2e8723fe71422c31b1e6ff3633aef
LouisVdwl <louis.vandewalle98@gmail.com>
2021-06-06 18:51:16 +0200

    cat done

--- TP6/src/Commands.java
+++ TP6/src/Commands.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -9,9 +12,37 @@ public class Commands extends ListenerAdapter {
                 event.getAuthor().getName() + ": " +
                 event.getMessage().getContentDisplay()
         );
+        String cmd = event.getMessage().getContentRaw();
+        String[] args = cmd.split(" ");
 
-        if (event.getMessage().getContentRaw().equals( prefix + " ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
+        if(args[0].equalsIgnoreCase(prefix)) {
+        	if (args[1].equals("ping")) {
+                event.getChannel().sendMessage("Pong!").queue();
+            }
+            
+            if (args[1].equals("dice")) {
+                Integer ret;
+                System.out.println(args.length);
+                if(args.length == 3) {
+                	ret = (int)(Math.random() * Integer.parseInt(args[2])) + 1;
+                    
+                }else {
+                	ret = (int)(Math.random() * 6) + 1;
+                }
+                event.getChannel().sendMessage("Résultat du dé à " + args[2] + " faces: " + ret).queue();
+            }
+            
+            if(args[1].equals("cat")) {
+            	EmbedBuilder res = new EmbedBuilder();
+                res.setTitle("Oh un chat !");
+                res.setImage(random(("https://cataas.com/c?")));
+                event.getChannel().sendMessage(res.build()).queue();
+            }
         }
+    }
+	
+	private static String random(String url) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return url + random.nextInt() + "=" + random.nextInt();
     }
 }
